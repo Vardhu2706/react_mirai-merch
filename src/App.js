314604b910a499a29f54d50222c57a49e4287f01
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-07-12 11:47:34 +0530

    Create basic homepage.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 // Importing Helpers
 import React from "react";
 
+// Importing Components
+import HomePage from "./Homepage.Component";
+
+// Functional Component
 const App = () => {
-  return <div>Test</div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 };
 
+// Default Export
 export default App;
---
+++ src/Homepage.Component.jsx
@@ -0,0 +1,46 @@
+// Importing Helpers
+import React from "react";
+
+// Importing Components
+import "./Homepage.Styles.scss";
+
+// Functional Component
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">JACKETS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">SHOES</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">WOMEN</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">MEN</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+// Default Export
+export default HomePage;
